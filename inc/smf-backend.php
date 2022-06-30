036295db6e9d27818cf568c4413df14f69da3dd7
Victor Wright <victor.wright@outlook.de>
2022-06-30 09:08:05 +0200

    Update smf-backend.php

--- inc/smf-backend.php
+++ inc/smf-backend.php
@@ -1,2 +1,2 @@
 <!-- ------------ IFRAME -------------- -->
-<iframe src="https://sharemyfood-backend.herokuapp.com/" title="SMF Backend" height="400"></iframe>
+<iframe src="https://sharemyfood-backend-v2.herokuapp.com/" title="SMF Backend" height="400"></iframe>
